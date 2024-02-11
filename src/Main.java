56e71f9adc81dc30d4f6842b9c4328314fa84e56
Milan Soucek <soucek_milan@centrum.cz>
2024-02-11 18:48:09 +0100

    ukol 5 bigdecimal

--- src/Main.java
+++ src/Main.java
@@ -19,19 +19,25 @@ public class Main {
         System.out.println(cena);
     }
 
-    public static void ukol5() {
-        var cena = 0.0;
+//    public static void ukol5() {
+//        var cena = 0.0;
 //         Nastavíme do proměnné hodnotu 0
 //         Desetkrát přičteme hodnotu jedna desetina (0,1):
-        for (int i = 0; i < 10; i++) {
-            cena += 0.1;
+//        for (int i = 0; i < 10; i++) {
+//            cena += 0.1;
 //                operátor "+=" znamená: přičti ke stávající
 //                hodnotě navíc 0.1
-        }
-        System.out.println(cena);
+//        }
+//        System.out.println(cena);
 //         Má vypsat "1.0". Ale vypíše "0.9999" !!!
+//    }
+    public static void ukol5() {
+    BigDecimal cena = BigDecimal.valueOf(0.0);
+    for (int i = 0; i < 10; i++) {
+        cena = cena.add(BigDecimal.valueOf(0.1));
+     }
+    System.out.println(cena);
     }
-    //
     public static void ukol6() {
         BigDecimal _cena = new BigDecimal ("11");
         _cena = _cena.add(BigDecimal.valueOf(0.1));
